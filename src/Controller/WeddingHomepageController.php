73a724328e13a38bf229514eae4bfb1aae5cd6ea
mateusz Kozioł <mateusz.koziol15@gmail.com>
2021-01-25 20:19:47 +0100

    Add password

--- src/Controller/WeddingHomepageController.php
+++ src/Controller/WeddingHomepageController.php
@@ -4,12 +4,35 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class WeddingHomepageController extends AbstractController
 {
-    public function homepage(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('wedding');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('access_page.html.twig', [
+            'error' => $error
+        ]);
+    }
+
+    /**
+     * @throws LogicException
+     */
+    public function logout(): void
+    {
+        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    public function wedding(): Response
     {
         return $this->render('wedding/homepage.html.twig');
     }
